ac20465f308522ac780e8e670539c79d86a2791b
Mohamed Kamal <ku18mz@gmail.com>
2024-02-18 20:48:59 +0200

    test

--- models/base_model.py
+++ models/base_model.py
@@ -23,10 +23,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             try:
                 kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                         '%Y-%m-%dT%H:%M:%S.%f')
