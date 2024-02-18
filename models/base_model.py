557375b7e1e08d723da5fb46d4a6d5961e0cd64b
Mohamed Kamal <ku18mz@gmail.com>
2024-02-18 20:48:16 +0200

    Refactor BaseModel initialization and add close method to storage classes

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
             try:
                 kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                         '%Y-%m-%dT%H:%M:%S.%f')
